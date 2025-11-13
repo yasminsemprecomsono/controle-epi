5f3d825a4f122ca454f2f50daa0addc70ad22ef8
yas <yasmin_sousa@estudante.sesisenai.org.br>
2025-11-13 20:53:05 -0300

    Initial commit

---
+++ src/main/java/com/sousa/controle_epi/ControleEpiApplication.java
@@ -0,0 +1,13 @@
+package com.sousa.controle_epi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sousa/controle_epi/ControleEpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sousa.controle_epi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
