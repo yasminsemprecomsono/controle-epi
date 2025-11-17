665bc0b8086fadcbf14599d07b0c5643154ba1ab
yas <yasmin_sousa@estudante.sesisenai.org.br>
2025-11-17 00:46:19 -0300

    feat: repositorios para o CRUD

--- src/main/java/com/sousa/controle_epi/repository/EmprestimoRepository.java
+++ src/main/java/com/sousa/controle_epi/repository/EmprestimoRepository.java
@@ -5,8 +5,6 @@ import com.sousa.controle_epi.entity.StatusEmprestimo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity, Long> {
 
