0a94fc7b5b20cc1c14ea96f620e397ce55f23848
yas <yasmin_sousa@estudante.sesisenai.org.br>
2025-11-17 15:18:38 -0300

    chore: removendo código de teste
    fiz os testes no main e depois restaurei

--- src/main/java/com/sousa/controle_epi/ControleEpiApplication.java
+++ src/main/java/com/sousa/controle_epi/ControleEpiApplication.java
@@ -1,13 +1,10 @@
 package com.sousa.controle_epi;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ControleEpiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ControleEpiApplication.class, args);
 	}
-
 }
